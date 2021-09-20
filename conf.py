552cffef7cb430ccf520df517157a068623b822b
Josh Mitchell <yoshanuikabundi@gmail.com>
2021-09-20 18:08:08 +1000

    Simplify file layout

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("./sphinx"))
 
 # -- Project information -----------------------------------------------------
 
@@ -71,13 +71,17 @@ nbsphinx_prolog = """
 """
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ["sphinx/_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
+exclude_patterns = [
+    "README.md",
+    "build",
+    "sphinx",
+    ".github",
+]
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -139,7 +143,7 @@ html_sidebars = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ["sphinx/_static"]
 
 html_css_files = [
     "notebooks.css",
